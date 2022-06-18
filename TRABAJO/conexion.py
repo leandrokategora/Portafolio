06780fdb5c6fa3ce14d77b20c7691fb2c33c0db3
Leandro Kategora <leandrokategora@gmail.com>
2022-06-18 11:19:52 -0300

    trade desde wsl todo al entorno windows para ver si es mas facil conectar a sql server

---
+++ TRABAJO/conexion.py
@@ -0,0 +1,19 @@
+from logging import exception
+import sqlalchemy as sa
+import urllib
+
+
+parameters = urllib.parse.quote_plus("DRIVER={SQL Server Native Client 11.0};"
+                                     "Server=WIN-SERVIDOR-BD\DWGRUPOTOTAL;"
+                                     "DATABASE=DW_Central;"
+                                     "UID=sa;"
+                                     "PWD=DWpln21")
+
+def run():
+    try:
+        engine = sa.create_engine("mssql+pyodbc:///?odbc_connect={}".format(parameters))
+        print("Conect succesfull")
+    except Exception:
+        print("Errora in DataBase conection")
+    
+
