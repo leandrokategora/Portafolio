54950c469863e295a2f1c22b1f91384d196dcf1b
Leandro Kategora <leandrokategora@gmail.com>
2022-09-29 16:18:02 -0300

    empece con el proyecto de hacer scraping

---
+++ TRABAJO/webscraper/main.py
@@ -0,0 +1,24 @@
+import argparse
+from common import config
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def _new_scraper(news_site_uid):
+    host = config()['news_sites'][news_site_uid]['url']
+
+    logging.info('Beginning scraper for {}'.format(host))
+ 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    
+    news_site_choices = list(config()['news_sites'].key())
+    parser.add_argument('news_site',
+                        help='The new site that you want to scrape',
+                        type=str,
+                        choices=news_site_choices)
+
+    args = parser.parse_args()
+    _new_scraper(args.news_site)
