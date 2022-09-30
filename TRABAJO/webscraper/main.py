6512c37fa9df070d370a27080a81a1b4f0cecf33
Leandro Kategora <leandrokategora@gmail.com>
2022-09-30 16:22:05 -0300

    ya funciona la base del scraper

--- TRABAJO/webscraper/main.py
+++ TRABAJO/webscraper/main.py
@@ -7,14 +7,14 @@ logger = logging.getLogger(__name__)
 
 
 def _new_scraper(news_site_uid):
-    host = config()['news_sites'][news_site_uid]['url']
+    host = config()['news_site'][news_site_uid]['url']
 
     logging.info('Beginning scraper for {}'.format(host))
  
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     
-    news_site_choices = list(config()['news_sites'].key())
+    news_site_choices = list(config()['news_site'].keys())
     parser.add_argument('news_site',
                         help='The new site that you want to scrape',
                         type=str,
